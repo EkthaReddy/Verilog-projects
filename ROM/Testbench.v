47ec57c65e376264c6ccd3e30c76fb7e005a0de2
Ektha Reddy <152515939+EkthaReddy@users.noreply.github.com>
2024-07-28 18:15:46 +0530

    Update Testbench.v

--- ROM/Testbench.v
+++ ROM/Testbench.v
@@ -1,1 +1,54 @@
 
+module rom_verilog_tb;
+  reg clk; 
+  reg en; 
+  reg [2:0] adr;
+  wire [2:0] data;
+  
+  rom r1(.clk(clk),.en(en),.adr(adr),.data(data));
+  
+  initial
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars(1);       
+      
+      clk=1'b1;
+      forever #5 clk = ~clk;
+    end
+  
+  initial
+    begin
+      en = 1'b1;
+      #10;                  
+      
+      en = 1'b0;      
+      adr = 3'b101;
+      #10;
+      
+      adr = 3'b011;
+      #10;
+      
+      adr = 3'b001;
+      #10;
+      
+      en = 1'b1;
+      adr = 3'b111;
+      #10;
+            
+      adr = 3'b100;
+      #10;
+      
+      en = 1'b1;
+      adr = 3'b000;
+      #10;
+      
+      adr = 3'bxxx;
+      #10;
+    end
+  
+  initial
+    begin
+      #80 $stop;
+    end
+  
+endmodule  
