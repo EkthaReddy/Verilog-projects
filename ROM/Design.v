da9f947c87f664c9ea99d34f558381ff8bb5f802
Ektha Reddy <152515939+EkthaReddy@users.noreply.github.com>
2024-07-28 18:15:27 +0530

    Update Design.v

--- ROM/Design.v
+++ ROM/Design.v
@@ -1,1 +1,27 @@
+  
+module rom_verilog ( input clk, input en, input [2:0] adr, output reg [2:0] data );
+  
+  reg [2:0] mem [7:0]; 
+  
+  always @ (posedge clk) 
+    begin
+      if (en)
+        data <= mem[adr];
+      else 
+        data <= 3'bxxx;
+    end
+  
+  initial 
+    begin    
+      mem[0] = 3'b000;
+      mem[1] = 3'b001;
+      mem[2] = 3'b010;
+      mem[3] = 3'b011;
+      mem[4] = 3'b100;
+      mem[5] = 3'b101;
+      mem[6] = 3'b110;
+      mem[7] = 3'b111;
+     
+    end    
 
+endmodule
